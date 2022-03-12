bd1cef0e58447d2a1d486a6c495d1eec93a6ba50
Nicki Lindstrom <lindstromn13@gmail.com>
2022-03-11 23:18:56 -0700

    styling to login updated & backend security added

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,10 @@
-const notFound = (req,res,next)=>{
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-}
-
-const errorHandler = (error,req,res,next) =>{
+const errorHandler = (error, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.sendStatus(statusCode);
     res.json({
         message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? null : error.stack
-    })
-}
+        stack: process.env.NODE_ENV === "production" ? null : error.stack,
+    });
+};
 
-module.exports = {notFound, errorHandler};
+module.exports = { errorHandler };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,41 +1,40 @@
-const { caretColor } = require('tailwindcss/defaultTheme');
-const defaultTheme = require('tailwindcss/defaultTheme');
+const { caretColor } = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    './src/**/*.js'
-  ],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    fontFamily:{
-      'display': ["Montserrat",...defaultTheme.fontFamily.sans]
+    purge: ["./src/**/*.js"],
+    darkMode: "class", // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            display: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        },
+        extend: {
+            gridTemplateColumns: {
+                "admin-table": "repeat(3, 3fr) 2fr",
+                "login-form": "30% 70%",
+            },
+            padding: {
+                "1/3": "33.33333%",
+                "9/16": "56.25%",
+                "2/3": "66.66667%",
+                "3/4": "75%",
+                full: "100%",
+            },
+            margin: {
+                nav: "68px",
+            },
+            minHeight: {
+                "nav-height": "calc(100vh - 68px)",
+            },
+            spacing: {
+                "4/3": "133.33333%",
+                "3/2": "150%",
+                "5/3": "166.66667%",
+            },
+        },
     },
-    extend: {
-      gridTemplateColumns: {
-        'admin-table': 'repeat(3, 3fr) 2fr',
-      },
-      padding: {
-        '1/3': '33.33333%',
-        '9/16': '56.25%',
-        '2/3': '66.66667%',
-        '3/4': '75%',
-        'full': '100%'
-      },
-      margin:{
-        'nav': '68px'
-      },
-      minHeight:{
-        'nav-height': 'calc(100vh - 68px)',
-      },
-      spacing: {
-        '4/3': '133.33333%',
-        '3/2': '150%',
-        '5/3': '166.66667%'
-      }
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
